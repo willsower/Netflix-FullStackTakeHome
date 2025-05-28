1b346dc20224d2d9205a1fd5167c3efaa6e00ea4
Taichen Rose <jatr812@gmail.com>
2025-05-28 10:52:58 -0700

    feat: Add mock data and interface for yelp business type

---
+++ client/__mocks__/bobaMock.ts
@@ -0,0 +1,54 @@
+import { type YelpBusiness } from '../src/interfaces/yelp';
+
+export const MOCK_BOBA_SHOPS: YelpBusiness[] = [
+  {
+    id: '1',
+    name: 'Seattle Best Tea',
+    rating: 4.5,
+    reviewCount: 662,
+    price: '$',
+    distance: 1000,
+    imageUrl:
+      'https://s3-media0.fl.yelpcdn.com/bphoto/Gq21goDeugDl0YLNwGtM6Q/348s.jpg',
+  },
+  {
+    id: '2',
+    name: 'Xing Fu Tang',
+    rating: 4.0,
+    reviewCount: 244,
+    price: '$$',
+    distance: 1500,
+    imageUrl:
+      'https://s3-media0.fl.yelpcdn.com/bphoto/IBsikEb3jsjfFgHEG3aJgw/348s.jpg',
+  },
+  {
+    id: '3',
+    name: 'ComeBuy Tea',
+    rating: 4.3,
+    reviewCount: 177,
+    price: '$$',
+    distance: 100,
+    imageUrl:
+      'https://s3-media0.fl.yelpcdn.com/bphoto/hwROmKiVou3Z9r804o2bZQ/l.jpg',
+  },
+  {
+    id: '4',
+    name: 'Gaga Tea',
+    rating: 4.6,
+    reviewCount: 99,
+    price: '$$',
+    distance: 500,
+    imageUrl:
+      'https://s3-media0.fl.yelpcdn.com/bphoto/iVg-FKI73FA6XNxZCdsRcw/348s.jpg',
+  },
+  {
+    id: '5',
+    name: 'Atulea',
+    rating: 4.3,
+    reviewCount: 352,
+    price: '$$',
+    distance: 1000,
+    imageUrl:
+      'https://s3-media0.fl.yelpcdn.com/bphoto/7BRgtG8X6xOssC9-4RafqA/348s.jpg',
+  },
+];
---
+++ client/src/interfaces/yelp.ts
@@ -0,0 +1,16 @@
+// These types are created from the Yelp Business Search Public API
+// Please see following Docs: https://docs.developer.yelp.com/reference/v3_business_search
+export interface YelpBusiness {
+  id: string;
+  name: string;
+  rating: number;
+  reviewCount: number;
+  price: string;
+  distance: number;
+  imageUrl: string;
+}
+
+export interface YelpSearchResponse {
+  businesses: YelpBusiness[];
+  total: number;
+}
