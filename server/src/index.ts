111ffee9f6a7d8c159db1ea67ce54f2ffc18a15d
Taichen Rose <jatr812@gmail.com>
2025-05-28 21:41:56 -0700

    feat: Add api for list businesses and add cors to the allowed domain

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,25 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
 
+import businessRoutes from './routes/businessRoutes';
+import { DEFAULT_PORT } from './constants/defaults';
+import { ALLOWED_ORIGINS } from './constants/common';
+
+dotenv.config();
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || DEFAULT_PORT;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ALLOWED_ORIGINS,
+    credentials: true,
+  })
+);
+
+app.use('/api/businesses', businessRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
